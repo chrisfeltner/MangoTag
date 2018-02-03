2d77e1c94f0cb7b38a84548a47a778e5b5e7291a
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-02-03 03:28:09 -0500

    initial commit

---
+++ MangoTag/MangoTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MangoTag
+//
+//  Created by Christopher Feltner on 2/3/18.
+//  Copyright © 2018 Christopher Feltner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
