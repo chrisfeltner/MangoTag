0f251bb4f5b675def6b561b2f11840f23b93c85a
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-02-03 23:19:14 -0500

    JoinGameViewController

--- MangoTag/MangoTag/ViewController.swift
+++ MangoTag/MangoTag/ViewController.swift
@@ -13,7 +13,8 @@ import FirebaseAuthUI
 
 class ViewController: UIViewController {
 
-  
+    var user: User?;
+    var uid: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +22,25 @@ class ViewController: UIViewController {
             else { return }
         let authViewController = authUI.authViewController()
         present(authViewController, animated: true)
-        
+        user = Auth.auth().currentUser
+        if(user != nil)
+        {
+            uid = user?.uid
+        }
+        else
+        {
+            uid = nil
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       // let destVC = segue.destination
+        
     }
 
 
